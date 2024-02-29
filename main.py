8f27b9b6eeb1894581ebdceb0cdc85a7d94d6c1e
BAX8453 <johannes.wegener@studium.uni-hamburg.de>
2024-03-01 00:02:03 +0100

    moved classes to utilities and finished basic game
    utilities contains classes and helper functions. main
    regards the primary loop and the game mechanics. may not
    be the best implementation to put all the logic in classes.
    beyond that, events should be looked more upon.
    also, we need to add more features - obviously.

--- main.py
+++ main.py
@@ -1,30 +1,72 @@
 from utilities import *
+# from pygame.locals import *
 
+
+# canvas init
 pygame.init()
+pygame.display.set_caption("End of Game Example")
 
 screen = pygame.display.set_mode((500, 500))
 clock = pygame.time.Clock()
-running = True
+font = pygame.font.Font(None, 36)
 
+RUNNING = True
+END = False
 height = screen.get_height()
 width = screen.get_width()
+num_balls = 5
 
 
+# object init
 player = Player(width/2, height/2)
-ball = Ball(width*r.random())
+balls = [Ball(width*r.random()) for _ in range(num_balls)]
+
+objects = [player] + balls
 
 
-while running:
+# logic loop
+while RUNNING:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            RUNNING = False
+        if event.type == CUSTOM_EVENT_TYPE:
+            print("END")
+            END = True
+            break
+    if END:
+        break
 
-    screen.fill('purple')
+    screen.fill((171, 101, 164))
     dt = clock.tick(60) / 100
 
-    player.update(dt, screen)
-    ball.update(dt, screen)
+    for i in objects:
+        i.update(dt, screen)
 
+    check_for_collisions(player, balls)
+
+    pygame.display.flip()
+
+
+button_x = width/2
+button_y = height*2/3
+button_width = 50
+button_height = 20
+button_stats = (button_x, button_y, button_width, button_height)
+
+while END:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(event.pos)
+            print(check_mouse_pos(event.pos, button_stats))
+        if event.type == pygame.MOUSEBUTTONDOWN and check_mouse_pos(event.pos, button_stats):
+            END = False
+    message = font.render("End of Game!", True, 'white')
+    screen.blit(message, (width/2-button_width/2, height/2))
+
+    exit_button = pygame.draw.rect(
+        screen, "red", (width/2-button_width/2, height*2/3, button_width, button_height))
+    exit_text = font.render("Exit", True, 'black')
+    screen.blit(exit_text, (width/2-button_width/2, height*2/3))
     pygame.display.flip()
 
 
--- utilities.py
+++ utilities.py
@@ -2,6 +2,31 @@ import pygame
 import random as r
 
 
+CUSTOM_EVENT_TYPE = pygame.USEREVENT + 1
+custom_event = pygame.event.Event(CUSTOM_EVENT_TYPE, message='custom event!')
+
+
+def check_for_collisions(player, objects):
+
+    for o in objects:
+        p_pos = player.get_pos()
+        o_pos = o.get_pos()
+        rel_pos = p_pos - o_pos
+        dist = rel_pos.length()
+
+        if dist < (player.radius + o.radius):
+            pygame.event.post(custom_event)
+
+
+def check_mouse_pos(pos0, button_stats):
+    bx, by, width, height = button_stats
+    if bx - width/2 < pos0[0] and pos0[0] < bx + width/2 and \
+            by - height/2 < pos0[1] and pos0[1] < by + height/2:
+        return True
+    else:
+        return False
+
+
 class Player:
     def __init__(self, x, y):
         self.x = x
@@ -22,7 +47,7 @@ class Player:
         if keys[pygame.K_d]:
             self.x += 25 * dt
 
-        pygame.draw.circle(screen, "black", self.get_pos(), self.radius)
+        pygame.draw.circle(screen, (39, 99, 90), self.get_pos(), self.radius)
 
 
 class Ball:
@@ -50,6 +75,13 @@ class Ball:
             pygame.draw.circle(screen, "white", self.get_pos(), self.radius)
 
         else:
-            if r.random() > 0.5:
+            if r.random() > 0.9:
                 self.alive = True
                 self.x = r.random() * screen.get_width()
+
+
+if __name__ == '__main__':
+    button_stats = (100, 100, 50, 50)
+    pos0 = (90, 100)
+
+    print(check_mouse_pos(pos0, button_stats))
