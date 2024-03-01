b063bbc3b728a8b452387526d98ad249a4a53e48
SilenceInTheBox <wegolus@gmail.com>
2024-03-01 23:02:50 +0100

    now a running game! definitely improvable but solid start

--- functions.py
+++ functions.py
@@ -11,6 +11,18 @@ def check_game_end(events):
             exit()
 
 
+def winning_message(screen, winning_player):
+    screen.fill((66, 135, 85))
+    font = pygame.font.Font(None, 36)
+    win_mes = font.render(
+        f'Congratulations {winning_player.name}! You won!', True, "white")
+    screen.blit(win_mes, (screen.get_width()//3, screen.get_height()//2))
+    pygame.display.flip()
+    while True:
+        events = pygame.event.get()
+        check_game_end(events)
+
+
 class Ground:
     def __init__(self, width, height) -> None:
         self.xs, self.ys = self.get_random_coords(width, height)
@@ -18,14 +30,13 @@ class Ground:
     def get_coords(self):
         return list(zip(self.xs, self.ys))
 
-    def get_random_coords(self, width, height, n=500):
+    def get_random_coords(self, width, height, n=101, num_supp=9):
         xs = [i*width/(n-1) for i in range(n)]
-        xsupp = xs[::n//9]
+        xsupp = [i*width/(num_supp-1) for i in range(num_supp)]
         ysupp = [0.5*height*r.random() for x in xsupp]
         CubInt = interpolate.CubicSpline(xsupp, ysupp)
         ys = CubInt(xs)
-        xs = [i for i in xs]
-        ys = [height - 0.8*height*i/max(abs(ys)) for i in ys]
+        ys = [height - 10 - (i-min(ys)) for i in ys]
         return xs, ys
 
     def draw(self, screen):
@@ -34,7 +45,8 @@ class Ground:
 
 
 class Player:
-    def __init__(self, x, y) -> None:
+    def __init__(self, name, x, y) -> None:
+        self.name = name
         self.x = x
         self.y = y
         self.alive = True
@@ -44,11 +56,18 @@ class Player:
     def get_pos(self):
         return pygame.Vector2(self.x, self.y)
 
+    def get_aim(self):
+        return pygame.Vector2(sin(self.aim), -cos(self.aim))
+
+    def get_shot_vec(self):
+        aim_vec = self.get_aim()
+        return aim_vec * self.power
+
     def draw(self, screen):
         pygame.draw.rect(screen, "red", (self.x-5, self.y-10, 10, 10))
 
     def draw_aim(self, screen):
-        arrow = pygame.Vector2(sin(self.aim), -cos(self.aim))
+        arrow = self.get_aim()
         start = (self.x + 50*arrow.x, self.y + 50*arrow.y)
         end = (self.x + 100*arrow.x, self.y + 100*arrow.y)
         pygame.draw.line(screen, 'red', start, end, width=2)
@@ -60,14 +79,70 @@ class Bullet:
         self.y = pos[1]
         self.vx = vel[0]
         self.vy = vel[1]
-        self.ay = -5
+        self.ay = 5
+
+        self.STATUS = None
 
     def get_pos(self):
         return pygame.Vector2(self.x, self.y)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, 'blue', self.get_pos(), 1)
+
+    def update_pos(self, dt):
+        self.vy += self.ay * dt
+        self.y += self.vy * dt
+        self.x += self.vx * dt
+
+    def check_ground_collision(self, ground):
+        ground_coords = ground.get_coords()
+        for (x0, y0), (x1, y1) in zip(ground_coords, ground_coords[1:]):
+            if x0 <= self.x <= x1:
+                if self.y > (y0 + y1)/2:
+                    return True
+                else:
+                    return False
+
+    def check_enemy_hit(self, enemy):
+        enmy_pos = enemy.get_pos()
+        own_pos = self.get_pos()
+        dist = (enmy_pos - own_pos).length()
+        if dist < 50:
+            return True
+        else:
+            return False
+
+    def check_out_of_bounds(self, screen):
+        x, y = self.get_pos()
+        width = screen.get_width()
+        height = screen.get_height()
+        if x < 0 or width < x or y < 0 or height < y:
+            return True
+        else:
+            return False
+
+    def simulate(self, screen, dt, ground, enemy):
+        j = 0
+        while True:
+            j += 1
+            self.update_pos(dt)
+            self.draw(screen)
+            if self.check_ground_collision(ground) and j > 150:
+                print('Shot collided with terrain.')
+                self.STATUS = 'GRD'
+                break
+            if self.check_enemy_hit(enemy):
+                print('Hit! Enemy destroyed.')
+                self.STATUS = 'HIT'
+                break
+            if self.check_out_of_bounds(screen):
+                print('Shot out of bounds...')
+                self.STATUS = 'OOB'
+                break
+            pygame.display.flip()
 
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
-    p1 = Player(100, 100)
-    None
+    b = Bullet((10, 10), (3, 3))
--- main.py
+++ main.py
@@ -26,56 +26,65 @@ PHYSICS_ACTIVE = False
 ground = Ground(width_px, height_px)
 
 # spawn players
-p1 = Player(ground.xs[100], ground.ys[100])
-p2 = Player(ground.xs[500-100], ground.ys[500-100])
+p1 = Player('Adam', ground.xs[20], ground.ys[20])
+p2 = Player('Eve', ground.xs[-20], ground.ys[-20])
+
+# init commands
+screen.fill((0, 0, 0))
+
+ground.draw(screen)
+p1.draw(screen)
+p2.draw(screen)
 
 
+active_player = p1 if TURN == 'p1' else p2
+inactive_player = p2 if TURN == 'p1' else p1
+
+# TURN loop - change aim, fire shot
 while RUNNING:
     check_game_end(pygame.event.get())
 
-    clock.tick(60)
     screen.fill((0, 0, 0))
 
     ground.draw(screen)
     p1.draw(screen)
     p2.draw(screen)
 
-    active_player = p1 if TURN == 'p1' else p2
+    active_player.draw_aim(screen)
 
-    # TURN loop
-    while True:
-        check_game_end(pygame.event.get())
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        active_player.aim -= .01
+    if keys[pygame.K_RIGHT]:
+        active_player.aim += .01
+    if keys[pygame.K_SPACE]:
+        # power measure
+        start_time = time.time()
+        while True:
+            events = pygame.event.get()
+            check_game_end(events)
+            dt = clock.tick(60) / 1000
+            if pygame.KEYUP in [i.type for i in events] and \
+                    pygame.K_SPACE in [i.key for i in events]:
+                end_time = time.time()
+                duration = end_time - start_time
+                active_player.power = 70*duration
 
-        clock.tick(60)
-        screen.fill((0, 0, 0))
-
-        ground.draw(screen)
-        p1.draw(screen)
-        p2.draw(screen)
-
-        active_player.draw_aim(screen)
-
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            active_player.aim -= .0005
-        if keys[pygame.K_RIGHT]:
-            active_player.aim += .0005
-        if keys[pygame.K_SPACE]:
-
-            # power measure
-            start_time = time.time()
-            while True:
-                events = pygame.event.get()
-                check_game_end(events)
-                if pygame.KEYUP in [i.type for i in events] and \
-                        pygame.K_SPACE in [i.key for i in events]:
-                    end_time = time.time()
-                    duration = end_time - start_time
-                    active_player.power = duration
+                shot = Bullet(active_player.get_pos(),
+                              active_player.get_shot_vec())
+                shot.simulate(screen, dt, ground, inactive_player)
+                if shot.STATUS in ['GRD', 'OOB']:
+                    active_player, inactive_player = inactive_player, active_player
+                    del shot
+                    break
+                elif shot.STATUS == 'HIT':
+                    RUNNING = False
+                    winning_message(screen, active_player)
                     break
 
-        pygame.display.flip()
-
+    # update display and cap frame rate
     pygame.display.flip()
+    clock.tick(60)
+
 
 pygame.quit()
